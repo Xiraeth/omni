8b3e730031ade50e2f73799c8217c6a751386e46
nick <nickiliadis99@gmail.com>
2024-12-14 13:38:34 +0200

    fix missing prop

--- app/finances/page.tsx
+++ app/finances/page.tsx
@@ -4,7 +4,12 @@ import { changeUrlParams } from "../common/functions/changeParams";
 const Finances = () => {
   return (
     <div className="flex flex-col items-center justify-center text-5xl fixed top-16 right-4 drop-shadow-lg shadow-black">
-      <LinkButton text="Home" href="/" className="relative left-4 top-4" />
+      <LinkButton
+        text="Home"
+        href="/"
+        className="relative left-4 top-4"
+        color="black"
+      />
       <button
         className="mt-8"
         onClick={() =>
