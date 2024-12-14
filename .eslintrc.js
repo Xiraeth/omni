819062254c48c2fbbb969b410c7ffe505ef142bf
nick <nickiliadis99@gmail.com>
2024-12-14 13:35:56 +0200

    fix lint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["next/core-web-vitals"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "no-unused-vars": "off",
+  },
+};
