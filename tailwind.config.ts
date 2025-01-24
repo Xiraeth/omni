149ed4994cfa7da1f058e4c23506349cf9beb072
nick <nickiliadis99@gmail.com>
2025-01-24 18:57:56 +0200

    Update code to match main branch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,7 @@ import { PluginAPI } from "tailwindcss/types/config";
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}"],
   darkMode: "class",
   theme: {
     extend: {
