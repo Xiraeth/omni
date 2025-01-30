93fe7ad96aa7d45ddb5f0dc0ab32955e9ad20d3d
nick <nickiliadis99@gmail.com>
2025-01-30 22:15:47 +0200

    try next-auth's built in protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,5 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+export { default } from "next-auth/middleware";
 
-export async function middleware(request: NextRequest) {
-  // const token = await getToken({ req: request });
-
-  // const isAuthPage =
-  //   request.nextUrl.pathname.startsWith("/login") ||
-  //   request.nextUrl.pathname.startsWith("/signup");
-
-  // // Protect these routes
-  // const protectedPaths = ["/todos", "/income", "/dashboard", "/expenses"];
-  // const isProtectedPath = protectedPaths.some((path) =>
-  //   request.nextUrl.pathname.startsWith(path)
-  // );
-
-  // if (!token && isProtectedPath) {
-  //   console.log("inside '!token' if");
-  //   console.log("token", token);
-  //   const url = new URL("/login", request.url);
-  //   url.searchParams.set("callbackUrl", request.nextUrl.pathname);
-  //   return NextResponse.redirect(url);
-  // }
-
-  // if (token && isAuthPage) {
-  //   return NextResponse.redirect(new URL("/", request.url));
-  // }
-
-  return NextResponse.next();
-}
+export const config = {
+  matcher: ["/income(.*)", "/expenses(.*)", "/todos(.*)"],
+};
