f470cefd3c5faab7c654fbdab165e65da448312d
nick <nickiliadis99@gmail.com>
2025-02-02 14:56:34 +0200

    add back button to reset-password page as well

--- app/reset-password/[id]/page.tsx
+++ app/reset-password/[id]/page.tsx
@@ -6,7 +6,7 @@ import { Button } from "@/components/ui/button";
 import useCustomToast from "@/hooks/useCustomToast";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
-import { LucideLoader } from "lucide-react";
+import { LucideArrowLeft, LucideLoader } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { Controller, useForm } from "react-hook-form";
 
@@ -64,6 +64,12 @@ const ResetPasswordPage = () => {
     <div
       className={`flex flex-col items-center justify-center h-screen w-full transition-all duration-150 text-dark dark:text-light font-lato`}
     >
+      <div className="absolute top-0 left-0 p-4">
+        <LucideArrowLeft
+          className="cursor-pointer"
+          onClick={() => router.push("/login")}
+        />
+      </div>
       <div className="w-[300px] sm:w-[500px] mx-auto bg-slate-200 dark:bg-slate-800 flex flex-col items-center justify-center gap-4 py-12 px-6 sm:px-20 h-fit drop-shadow-lg shadow-black rounded-lg transition-all duration-150 dark:text-white">
         <p className="text-xl font-bold">Enter your new password</p>
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
